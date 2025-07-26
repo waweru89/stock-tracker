a865562d89bb53ecc7a6ab4e3c51a46b798a360a
waweru89 <wawerumburu157@gmail.com>
2025-07-26 15:29:26 +0300

    changes

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const { createClient } = supabase;
 
-const supabaseUrl = 'https://YOUR-PROJECT.supabase.co';
-const supabaseKey = 'YOUR-ANON-KEY';
+REACT_APP_SUPABASE_URL=http;//qkdevvlemcuwynbkspsk.supabase.co
+REACT_APP_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrZGV2dmxlbWN1d3luYmtzcHNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MTg1MTksImV4cCI6MjA2OTA5NDUxOX0.znePO0GumUsZkkKbaLcwuVO9lHq90YtMl0ZuL0hanVw
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
